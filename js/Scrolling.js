620debc6c9bf3a1f1c5653e3126c48c20f44d3d4
sainath378 <92171156+sai-na@users.noreply.github.com>
2023-02-16 00:16:37 +0530

    news added

--- js/Scrolling.js
+++ js/Scrolling.js
@@ -1,7 +1,7 @@
 $(function () {
     var tickerLength = $('.container2 ul li').length
     var tickerHeight = $('.container2 ul li').outerHeight()
-    $('.container2 ul li:last-child').prependTo('.container ul')
+    $('.container2 ul li:last-child').prependTo('.container2 ul')
     $('.container2 ul').css('marginTop', -tickerHeight)
     function moveTop() {
         $('.container2 ul').animate({
@@ -14,4 +14,25 @@ $(function () {
     setInterval(function () {
         moveTop()
     }, 3000)
+})
+
+
+
+
+$(function () {
+    var tickerLength = $('.container3 ul li').length
+    var tickerHeight = $('.container3 ul li').outerHeight()
+    $('.container3 ul li:last-child').prependTo('.container3 ul')
+    $('.container3 ul').css('marginTop', -tickerHeight)
+    function moveTop() {
+        $('.container3 ul').animate({
+            top: -tickerHeight
+        }, 600, function () {
+            $('.container3 ul li:first-child').appendTo('.container3 ul')
+            $('.container3 ul').css('top', '')
+        })
+    }
+    setInterval(function () {
+        moveTop()
+    }, 5000)
 })
