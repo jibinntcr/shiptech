c965ecebaafa8627b3bebce54b4b56b40bb1206a
sainath378 <92171156+sai-na@users.noreply.github.com>
2023-02-25 23:35:28 +0530

    scrolling height

--- js/Scrolling.js
+++ js/Scrolling.js
@@ -1,6 +1,10 @@
 $(function () {
     var tickerLength = $('.container2 ul li').length
     var tickerHeight = $('.container2 ul li').outerHeight()
+    tickerHeight += 24
+    console.log("tickerHeight")
+    console.log(tickerHeight)
+
     $('.container2 ul li:last-child').prependTo('.container2 ul')
     $('.container2 ul').css('marginTop', -tickerHeight)
     function moveTop() {
@@ -22,6 +26,8 @@ $(function () {
 $(function () {
     var tickerLength = $('.container3 ul li').length
     var tickerHeight = $('.container3 ul li').outerHeight()
+    tickerHeight += 24
+
     $('.container3 ul li:last-child').prependTo('.container3 ul')
     $('.container3 ul').css('marginTop', -tickerHeight)
     function moveTop() {
