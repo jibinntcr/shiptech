78dd012c832e456c753e8bf18c23e55d1dfcc772
jerry sabu <jerry@lanwaresolutions.com>
2023-04-04 16:01:39 +0530

    library issus fixed

--- library.php
+++ library.php
@@ -14,8 +14,7 @@
     <!-- Google Web Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&family=Roboto:wght@500;700&display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&family=Roboto:wght@500;700&display=swap" rel="stylesheet">
 
     <!-- Icon Font Stylesheet -->
     <!-- <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet"> -->
@@ -37,8 +36,7 @@
 
 <body>
     <!-- Spinner Start -->
-    <div id="spinner"
-        class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+    <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
         <div class="spinner-grow text-primary" style="width: 3rem; height: 3rem;" role="status">
             <span class="sr-only">Loading...</span>
         </div>
@@ -71,8 +69,7 @@
             <div class="row g-5">
                 <div class="col-lg-6 ps-lg-0 wow fadeInLeft" data-wow-delay="0.1s" style="min-height: 400px;">
                     <div class="position-relative h-100">
-                        <img class="position-absolute img-fluid w-100 h-100" src="img/lbrary-image.jpg"
-                            style="object-fit: cover; border-radius: 20px;" alt="">
+                        <img class="position-absolute img-fluid w-100 h-100" src="img/lbrary-image.jpg" style="object-fit: cover; border-radius: 20px;" alt="">
                     </div>
                 </div>
                 <div class="col-lg-6 about-text wow fadeInUp ship-contact-data justify-para" data-wow-delay="0.3s">
@@ -92,8 +89,7 @@
                         <div class="mb-5" style=" margin-left:15px;">
                             <div class="row mb-md-3 mb-2">
                                 <div class="col-2"> <i class="fa fa-envelope "></i></div>
-                                <div class="col-10 "><a class="white col-11"
-                                        href="mailto:librarydstcusat@gmail.com">librarydstcusat@gmail.com</a></div>
+                                <div class="col-10 "><a class="white col-11" href="mailto:librarydstcusat@gmail.com">librarydstcusat@gmail.com</a></div>
                             </div>
                             <div class="row mb-md-3 mb-2">
                                 <div class="col-2"> <i class="fa fa-phone "></i></div>
@@ -107,13 +103,12 @@
                         60
                         CUSAT Digital library offers access to past question papers, news items, syllabuses and other
                         e-contents. Past B.Tech Naval Architecture
-                        <a target="_blank" href="http://dspace.cusat.ac.in/jspui/handle/123456789/6731"
-                            style="text-decoration-line: underline;"> Question papers </a>
+                        <a target="_blank" href="http://dspace.cusat.ac.in/jspui/handle/123456789/6731" style="text-decoration-line: underline;"> Question papers </a>
                         are available in the digital library.
                     </p>
                 </div>
                 <div class="container-xxl py-5">
-                    <div class="container py-5">
+                    <div class="container">
                         <div class="row g-5 align-items-start">
                             <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
                                 <div class="col-lg-12 feature-text wow fadeInUp" data-wow-delay="0.1s">
@@ -125,8 +120,7 @@
                                                 <p class="justify-para">
                                                     The Library remains open on all working days of the Department. It
                                                     remains closed on even Saturdays, Sundays and other Holidays. The
-                                                    Library is open from <span
-                                                        style="color:#ff3e41 ; font-weight: bold; "> 9AM to 4PM</span>
+                                                    Library is open from <span style="color:#ff3e41 ; font-weight: bold; "> 9AM to 4PM</span>
                                                 </p>
                                             </div>
                                         </div>
@@ -144,8 +138,7 @@
                                                 <h5>Dr.Sheeja N.K. (PhD)</h5>
                                                 <div class="row mb-md-3 mb-2">
                                                     <div class="col-2"> <i class="fa fa-envelope "></i></div>
-                                                    <div class="col-10 "><a class="white col-11"
-                                                            href="mailto:sheeja@cusat.ac.in">sheeja@cusat.ac.in</a>
+                                                    <div class="col-10 "><a class="white col-11" href="mailto:sheeja@cusat.ac.in">sheeja@cusat.ac.in</a>
                                                     </div>
                                                 </div>
                                             </div>
@@ -160,7 +153,7 @@
 
 
                 <div class="container-xxl py-5">
-                    <div class="container py-5">
+                    <div class="container">
                         <!-- <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
                             <h6 class="text-secondary text-uppercase">Department of Shiptechnology</h6>
                             <h1 class="mb-5">Courses Offered</h1>
@@ -169,39 +162,31 @@
                             <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.3s">
                                 <div class="service-item p-4 fac-img-curv">
                                     <div class="overflow-hidden mb-4">
-                                        <img class="fac-img-curv img-fluid" src="./img/TheNavalArchitectMagazine.jpg"
-                                            style="object-fit: cover; height: 240px; " alt="">
+                                        <img class="fac-img-curv img-fluid" src="./img/TheNavalArchitectMagazine.jpg" style="object-fit: cover; height: 240px; " alt="">
                                     </div>
                                     <h4 class="mb-3">The Naval Architect Magazine</h4>
-                                    <a target="_blank" class="btn-slide mt-2"
-                                        href="https://www.rina.org.uk/Magazines.html"><i
-                                            class="fa fa-arrow-right"></i><span>Read
+                                    <a target="_blank" class="btn-slide mt-2" href="https://www.rina.org.uk/Magazines.html"><i class="fa fa-arrow-right"></i><span>Read
                                             More</span></a>
                                 </div>
                             </div>
                             <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.5s">
                                 <div class="service-item p-4 fac-img-curv">
                                     <div class="overflow-hidden mb-4">
-                                        <img class="img-fluid fac-img-curv" src="img/nptel.jpg" alt=""
-                                            style="object-fit: cover; height: 240px; ">
+                                        <img class="img-fluid fac-img-curv" src="img/nptel.jpg" alt="" style="object-fit: cover; height: 240px; ">
                                     </div>
                                     <h4 class="mb-3">Naval Architecture Online Classes</h4>
                                     <a target="blank
-                                    " class="btn-slide mt-2"
-                                        href="https://www.youtube.com/channel/UCVM3rFd2pDbuf_u7WILEJpw"><i
-                                            class="fa fa-arrow-right"></i><span>Read
+                                    " class="btn-slide mt-2" href="https://www.youtube.com/channel/UCVM3rFd2pDbuf_u7WILEJpw"><i class="fa fa-arrow-right"></i><span>Read
                                             More</span></a>
                                 </div>
                             </div>
                             <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.7s">
                                 <div class="service-item p-4 fac-img-curv">
                                     <div class="overflow-hidden mb-4">
-                                        <img class="img-fluid fac-img-curv" src="img/dig-librrary.jpg" alt=""
-                                            style="object-fit: cover; height: 240px; ">
+                                        <img class="img-fluid fac-img-curv" src="img/dig-librrary.jpg" alt="" style="object-fit: cover; height: 240px; ">
                                     </div>
                                     <h4 class="mb-3">NA &amp; SB Question <br>Papers</h4>
-                                    <a target="_blank" class="btn-slide mt-2" href="http://dspace.cusat.ac.in/ "><i
-                                            class="fa fa-arrow-right"></i><span>Read
+                                    <a target="_blank" class="btn-slide mt-2" href="http://dspace.cusat.ac.in/ "><i class="fa fa-arrow-right"></i><span>Read
                                             More</span></a>
                                 </div>
                             </div>
@@ -209,26 +194,6 @@
                         </div>
                     </div>
                 </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
                 <!-- <div class="container-xxl py-5">
                     <div class="container py-5">
                         <div class="row g-4 flex-wrap justify-content-center">
