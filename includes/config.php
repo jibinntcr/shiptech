5744ffbdc86446f3ff338e033098e7eea9b28bbe
jerry sabu <jerry@lanwaresolutions.com>
2023-04-09 19:15:43 +0530

    Merge branch 'dashboard' of https://github.com/jibinntcr/shiptech into dashboard

--- includes/config.php
+++ includes/config.php
@@ -4,7 +4,7 @@
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'infinio_shipdb');
+define('DB_NAME', 'shiptech');
 //echo "connecdted ";
 // Establish database connection.
 try {
