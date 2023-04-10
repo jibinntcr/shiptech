2a9623e18a96d01ad62d8d63c77d11a59fe92c53
KD <krishnadas19130388@ssmpoly.ac.in>
2023-04-10 08:02:59 +0400

    Revert "update"
    This reverts commit f3ccfd6cf5298c588358d59b94eb2a765e9051b5.

--- index.php
+++ index.php
@@ -63,6 +63,50 @@ include('includes/config.php');
     }
 
     .card1 {
+  display: block;
+  position: relative;
+  height: 100%;
+  max-width: 262px;
+  background-color: #f2f8f9;
+  border-radius: 4px;
+  padding: 32px 24px;
+  margin: 12px;
+  text-decoration: none;
+  z-index: 0;
+  overflow: hidden;
+  background: #abd;
+
+  &:before {
+    content: "";
+    position: absolute;
+    z-index: -1;
+    top: -16px;
+    right: -16px;
+    background: #00838d;
+    height: 32px;
+    width: 32px;
+    border-radius: 32px;
+    transform: scale(1);
+    transform-origin: 50% 50%;
+    transition: transform 0.25s ease-out;
+  }
+
+  &:hover:before {
+    transform: scale(24);
+  }
+}
+
+.card1:hover {
+  p {
+    transition: all 0.3s ease-out;
+    color: rgba(255, 255, 255, 0.8);
+  }
+  h3 {
+    transition: all 0.3s ease-out;
+    
+    color: #ffffff;
+  }
+}
 
 
     </style>
