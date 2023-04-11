69a56e646e4802650a860f6f05b1c4584cc657f5
jerry sabu <jerry@lanwaresolutions.com>
2023-04-11 09:00:07 +0530

    events slug added

--- dash.shiptech.cusat/new-events.php
+++ dash.shiptech.cusat/new-events.php
@@ -15,7 +15,10 @@ if (strlen($_SESSION['alogin']) == 0) {
         move_uploaded_file($_FILES['file']['tmp_name'], $file);
         $photo = basename($_FILES["file"]["name"]);
         $status = '1';
-        $sql = "INSERT INTO events(title,content,date,status,image) VALUES ('" . $heading . "','" . $content . "','" . $date . "','" . $status . "','" . $photo . "')";
+
+        $slug = strtolower(preg_replace('/[^a-zA-Z0-9\-]/', '', preg_replace('/\s+/', '-', $heading)));
+
+        $sql = "INSERT INTO events(title,content,date,status,image,slug) VALUES ('" . $heading . "','" . $content . "','" . $date . "','" . $status . "','" . $photo . "','" . $slug . "')";
         $query = $dbh->prepare($sql);
         $result = $query->execute();
         if ($query->rowCount() > 0) {
